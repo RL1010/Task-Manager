de5f8f39d1384ea023abf8fb3cb3574901481755
Riola <riolalama@live.com>
2021-12-06 13:44:14 +0100

    task-manager

---
+++ errors/custom-error.js
@@ -0,0 +1,13 @@
+class CustomAPIError extends Error{
+    constructor(message, status){
+        super(message)
+        this.statusCode = status;
+    }
+
+}
+
+const createCustomError = (msg, statusCode) => {
+return new CustomAPIError(msg, statusCode)
+}
+
+module.exports = {createCustomError, CustomAPIError}
